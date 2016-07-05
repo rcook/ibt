095dd23b1f578536914a13c88a7841469bd45dba
Richard Cook <rcook@rcook.org>
2016-07-05 15:20:05 -0700

    Pass remaining command-line arguments to aliased command

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     cout << "Hello world" << endl;
+    cout << "argc=" << argc << endl;
+    for (int i = 0; i < argc; ++i)
+    {
+        cout << "argv[" << i << "] = " << argv[i] << endl;
+    }
 }
