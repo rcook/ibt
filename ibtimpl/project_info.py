3c8b30a3046ae2c74a216752e360e0e60cd3958d
Richard Cook <rcook@rcook.org>
2016-09-21 20:15:43 -0700

    Dump out list of paths probed if no Ibtfile located

--- ibtimpl/project_info.py
+++ ibtimpl/project_info.py
@@ -8,6 +8,7 @@
 ###############################################################################
 
 import os
+import sys
 import yaml
 
 # Detects current project directory and configuration path based on following
@@ -23,13 +24,14 @@ class ProjectInfo(object):
 
     @staticmethod
     def read(dir):
-        def find_matching_config_path(projects, original_dir):
+        def find_matching_config_path(locations, projects, original_dir):
             user_dir = os.path.expanduser("~")
             longest_prefix = os.path.abspath("/")
             longest_dir = None
             longest_config_path = None
             for key in projects:
                 dir = os.path.abspath(os.path.join(user_dir, key))
+                locations.append(dir)
                 prefix = os.path.commonprefix([dir, original_dir])
                 if prefix == dir and len(prefix) > len(longest_prefix):
                     longest_prefix = prefix
@@ -37,8 +39,9 @@ class ProjectInfo(object):
                     longest_config_path = os.path.abspath(os.path.join(user_dir, projects[key]))
             return longest_dir, longest_config_path
 
-        def read_config(original_dir):
+        def read_config(locations, original_dir):
             user_project_path = os.path.expanduser("~/.ibtprojects")
+            locations.append(user_project_path)
             if not os.path.isfile(user_project_path):
                 return None
 
@@ -49,24 +52,30 @@ class ProjectInfo(object):
             if projects is None:
                 return None
 
-            dir, config_path = find_matching_config_path(projects, original_dir)
+            dir, config_path = find_matching_config_path(locations, projects, original_dir)
             if dir is None:
                 return None
 
             return ProjectInfo(dir, config_path)
 
-        def read_helper(original_dir, dir):
+        def read_helper(locations, original_dir, dir):
             config_path = os.path.join(dir, "Ibtfile")
+            locations.append(config_path)
             if os.path.isfile(config_path):
                 return ProjectInfo(dir, config_path)
 
             parent_dir = os.path.dirname(dir)
             if parent_dir == dir:
-                return read_config(original_dir)
+                return read_config(locations, original_dir)
             else:
-                return read_helper(original_dir, parent_dir)
+                return read_helper(locations, original_dir, parent_dir)
 
-        return read_helper(dir, dir)
+        locations = []
+        result = read_helper(locations, dir, dir)
+        if not result:
+          for location in locations:
+            sys.stderr.write("> Probed for configuration at {}\n".format(location))
+        return result
 
     @property
     def dir(self): return self._dir
