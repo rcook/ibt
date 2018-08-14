fc9c1002e46edddaf467615b2ec11c53609b8da3
Richard Cook <rcook@rcook.org>
2018-08-14 14:30:20 -0700

    Bump to version 0.7

--- ibt/__init__.py
+++ ibt/__init__.py
@@ -8,5 +8,5 @@
 ###############################################################################
 
 __project_name__ = "ibt"
-__version__ = "0.6"
+__version__ = "0.7"
 __description__ = "Simple wrappers around Docker etc. for fully isolated build environments"
